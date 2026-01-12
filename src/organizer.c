838dcba86baf97c0a90889e72d245e3fb09b8b7a
LeoBasan <150547432+LeoBasan@users.noreply.github.com>
2026-01-12 12:49:57 -0300

    Initial file organizer
    - Organizes files into folders based on file extension
    - Creates target directories automatically
    - Handles files and folders correctly on Windows
    - Initial functional version

--- src/organizer.c
+++ src/organizer.c
@@ -5,6 +5,65 @@
 
 #include "organizer.h"
 
+// Funcion para obtener extension del archivo
+static char* get_extension(char* fileName){
+    char *dot;
+
+    // Busco el ultimo .
+    dot = strrchr(fileName, '.');
+    // Si no encuentro un .
+    if(dot == NULL){
+        return NULL;
+    }
+    // Si el . es el primer caracter (.env, .gitignore)
+    if(dot == fileName){
+        return NULL;
+    }
+    // Si el . es el ultimo caracter (file.)
+    if(*(dot+1) == '\0'){
+        return NULL;
+    }
+    // Sino devuelvo lo que esta despues del punto
+    return dot+1;
+}
+
+// Funcion para mover archivos
+static void handle_file(const char *base_path, const char *filename){
+    char *ext = get_extension((char*)filename);
+
+    if(!ext){
+        return;
+    }
+    // Armo el path del destino
+    char target_dir[1024];
+    snprintf(target_dir, sizeof(target_dir), "%s/%s", base_path, ext);
+
+    struct stat st;
+    if(stat(target_dir, &st) == -1){
+        // no existe la carpeta asi que la creo
+        if(mkdir(target_dir) == -1){
+            perror("mkdir");
+            return;
+        } 
+    }
+
+    char source_path[1024];
+    snprintf(source_path, sizeof(source_path), "%s/%s", base_path, filename);
+
+    // Como no se si el target dir es mas grande verifico el retorno y lo salteo sino
+    char target_path[1024];
+    int written = snprintf(target_path, sizeof(target_path), "%s/%s", target_dir, filename);
+    if(written < 0 || written>= (int)sizeof(target_path)){
+        fprintf(stderr, "Ruta demasiado larga, se omite el archivo\n");
+        return;
+    }
+
+    if(rename(source_path, target_path) == -1){
+        perror("rename");
+    }
+    //printf("Archivo %s tiene extension %s\n", filename, ext);
+}
+
 void organize_files(const char *path){
     DIR *dir;
     struct dirent *entry;
@@ -35,10 +94,12 @@ void organize_files(const char *path){
         if(S_ISDIR(info.st_mode)){
             printf("[DIR ] %s\n", entry->d_name);
         }else if(S_ISREG(info.st_mode)){
-            printf("[FILE ] %s\n", entry->d_name);
+            handle_file(path, entry->d_name);
         }
         
     }
+
+    // printf("Ext: %s\n", get_extension("foto.png"));
 
     // 3. Cierro el directorio
     closedir(dir);
